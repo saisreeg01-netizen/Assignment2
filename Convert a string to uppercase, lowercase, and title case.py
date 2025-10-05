6438bb0febecced34641897eea0df27a88e44914
Sai <saisreeg01@gmail.com>
2025-10-05 13:00:58 +0530

    Intial commit

---
+++ Convert a string to uppercase, lowercase, and title case.py
@@ -0,0 +1,12 @@
+string = "Hello World"
+
+uppercase_string = string.upper()
+print("Uppercase:", uppercase_string)
+
+lowercase_string = string.lower()
+print("Lowercase:", lowercase_string)
+
+title_case_string = string.title()
+print("Title Case:", title_case_string)
+
+
